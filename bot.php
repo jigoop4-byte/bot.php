67b09f854e1c0c355ff33e94cdaa461d39dc84d2
jigoop4-byte <jigoop4@gmail.com>
2025-09-05 01:37:07 +0530

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,10 +1,2 @@
 <?php
-$update = json_decode(file_get_contents("php://input"), true);
-
-$chat_id = $update["message"]["chat"]["id"] ?? "";
-$msg = $update["message"]["text"] ?? "";
-
-if ($msg) {
-    file_get_contents("https://api.telegram.org/bot8343483650:AAE4TAKAM5BG6ovBxXlofpdT9NPlXO5r2Jw/sendMessage?chat_id=$chat_id&text=👉 $msg 👈");
-}
-?>
+echo "Bot is running!";
